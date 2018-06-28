728a222a54ab9d6afdc22d43721da51032f77fed
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-06-28 13:02:37 -0500

    Merge Sort

---
+++ merge-sort-array.js
@@ -0,0 +1,58 @@
+/* MERGE SORT - ALWAYS N LOG N
+  iterate array and compare adjacent values.
+  swap values if left greater than right.
+  call sort again if a swap has occurred, otherwise sort complete
+
+  input   1, 3, 4, 2, 6, 5
+  output  1, 2, 3, 4, 5, 6
+
+  Best: O(n) - array is already in order
+  Worst: O(n^2) - every value has to be swapped
+  Avg: O(n^2)
+
+*/
+
+'use strict';
+
+function merge(left, right, array) {
+  let leftIndex = 0;
+  let rightIndex = 0;
+  let outputIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      array[outputIndex++] = left[leftIndex++];
+    }
+    else {
+      array[outputIndex++] = right[rightIndex++];
+    }
+  }
+
+  for (let i = leftIndex; i < left.length; i++) {
+    array[outputIndex++] = left[i];
+  }
+
+  for (let i = rightIndex; i < right.length; i++) {
+    array[outputIndex++] = right[i];
+  }
+  return array;
+}
+
+
+function mergeSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const middle = Math.floor(array.length / 2);
+  let left = array.slice(0, middle);
+  let right = array.slice(middle, array.length);
+
+  left = mergeSort(left);
+  right = mergeSort(right);
+  return merge(left, right, array);
+}
+
+let data = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5];
+console.log(' Input: ', data);
+mergeSort(data);
+console.log('Output: ', data);
