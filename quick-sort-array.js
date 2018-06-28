7d3164b498450774e99f8c947f27390c71c8acf6
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-06-28 11:13:31 -0700

    added count

--- quick-sort-array.js
+++ quick-sort-array.js
@@ -22,6 +22,7 @@ function partition(array, start, end) {
 }
 
 function quickSort(array, start = 0, end = array.length) {
+  quickCount++; 
   if (start >= end) {
     return array; // if wall is equal or greater to the end index return the array because its already sorted
   }
@@ -32,6 +33,8 @@ function quickSort(array, start = 0, end = array.length) {
 }
 
 let data = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5];
+let quickCount = 0;
 console.log(' Input: ', data);
 quickSort(data);
 console.log('Output: ', data);
+console.log(quickCount);
